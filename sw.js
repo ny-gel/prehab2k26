6e38ebcb908e228eb1d676070aabfb81853ccd29
ny-gel <220466965+ny-gel@users.noreply.github.com>
2026-06-12 11:21:11 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'prehab-v11';
+const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.json', '/logo.png', '/logo-prehab.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
